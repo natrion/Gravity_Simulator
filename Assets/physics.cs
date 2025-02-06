2c0d3557f25531c3ba6510bb7b95f3d9c3cf1cc3
natrion <109035902+natrion@users.noreply.github.com>
2025-02-06 16:14:01 +0100

    gravity added
    after definitely no mistakes

--- Assets/physics.cs
+++ Assets/physics.cs
@@ -11,41 +11,31 @@ public class physics : MonoBehaviour
         return rotation * Vector3.forward; // Aplikujeme rotáciu na vektor (0,0,1)
     }
 
-    public  List<Vector3> positions;
+    public Vector3[] positions;
     public int spawnAmount = 20;
     public float SpacePerAmount = 1;
     void spawnPoints()
     {
-        positions = new List<Vector3>();
+        positions = new Vector3[spawnAmount];
         float totalSpaceRadius = Mathf.Pow((SpacePerAmount * spawnAmount) / ((4f / 3f) * Mathf.PI), 1f / 3f);
         for (int i = 0; i < spawnAmount; i++)
         {
-            positions.Add(Random.onUnitSphere * Mathf.Pow( Random.RandomRange(0f, 1f),1f/3f) * totalSpaceRadius);
+            positions[i] = Random.onUnitSphere * Mathf.Pow( Random.RandomRange(0f, 1f),1f/3f) * totalSpaceRadius;
         }
     }
     public Mesh pointMesh;
     public Material pointMaterial;
     public float pointSize = 0.1f;
+    public float pointMass = 1;
+    public float GStrenght = 1;
+
+
     void visualizatePositions()
     {
-        /*
-        //inicializating variables
-        Matrix4x4[] pointsTRS = new Matrix4x4[positions.Count];
-        int Poslen = pointsTRS.Length;
-        Quaternion pointRot = Quaternion.EulerRotation(Vector3.zero);
-        print(pointRot.x+ " "+ pointRot.y+ " "+ pointRot.z+" "+ pointRot.w);
-        Vector3 pointSizeVector = Vector3.one * pointSize;
-        //converting to positions Metrix
-        for (int i = 0; i < pointsTRS.Length; i++)
-        {
-            Matrix4x4 transform = Matrix4x4.TRS(positions[i], pointRot, pointSizeVector);
-            pointsTRS[i] = transform;
-        }
-        //drawing meshes
-        Graphics.DrawMeshInstanced(pointMesh, 0, pointMaterial, pointsTRS, Poslen);
-        */
+        
+       
         //declearing buffers
-        int positionsNum = positions.Count;
+        int positionsNum = positions.Length;
         ComputeBuffer inPositionsBuffer = new ComputeBuffer(positionsNum, sizeof(float) * 3);
         inPositionsBuffer.SetData(positions);
         ComputeBuffer outPositionsBuffer = new ComputeBuffer(positionsNum, sizeof(float) * 3);
@@ -56,13 +46,18 @@ public class physics : MonoBehaviour
         physicsCom.SetBuffer(mainKernel, "positionsIn", inPositionsBuffer);
         physicsCom.SetBuffer(mainKernel, "positionsOut", outPositionsBuffer);
         physicsCom.SetBuffer(mainKernel, "MetrixTransforms", outMetrixTransformBuffer);
-        //dispatch
-        physicsCom.SetFloat("size", pointSize);
 
-        //taking data from dispach
+        //setting data for dispach
+        physicsCom.SetFloat("size", pointSize);
+        physicsCom.SetFloat("GStrenght", GStrenght);
+        physicsCom.SetFloat("pointMass", pointMass);
+        physicsCom.SetFloat("frameLenght", Time.deltaTime);
+        //dispatch
         physicsCom.Dispatch(mainKernel, Mathf.CeilToInt(positionsNum / 64f), 1, 1);
-        posOut = new Vector3[positionsNum];
-        outPositionsBuffer.GetData(posOut);
+        
+        //taking data from dispach
+        positions = new Vector3[positionsNum];
+        outPositionsBuffer.GetData(positions);
 
         Matrix4x4[] pointsTRS = new Matrix4x4[positionsNum];
         outMetrixTransformBuffer.GetData(pointsTRS);
@@ -70,14 +65,12 @@ public class physics : MonoBehaviour
         //drawing meshes
         Graphics.DrawMeshInstanced(pointMesh, 0, pointMaterial, pointsTRS, positionsNum);
     }
-    public Vector3[] posOut;
     private void Start()
     {
-   
+        spawnPoints();
     }
     void Update()
     {
-        spawnPoints();
         visualizatePositions();
     }
 }
